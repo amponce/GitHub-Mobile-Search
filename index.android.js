18d0368e579a13d204a5a91b5d9f658bb07849f7
Aaron Poncé <aaron.m.ponce@gmail.com>
2016-11-07 15:47:14 -0500

    Adding nativebaseTutorial files to repo

---
+++ index.android.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+// nativebaseTutorial by GeekyAnts
+
+import App from './app';
+export default App;
---
+++ index.ios.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+
+
+// nativebaseTutorial by GeekyAnts
+
+import App from './app';
+export default App;
+
